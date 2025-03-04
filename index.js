2bc7e7537c2733a13d72e5064f38046a44dd04af
Rakibul Hasan <rakibulhasan745101@gmail.com>
2025-03-04 21:06:01 +0600

    mongodb connected successfully and environment variable is setup perfectly

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 import express from "express";
+import { client } from "./src/db/db.config.js";
+import 'dotenv/config'
+
+const app = express();
+const port = process.env.PORT || 2345
+
+app.get('/', (req, res) => {
+    console.log('the server is running!!');
+    res.send('The app is mine.')
+})
+
+app.listen(port, async () => {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        console.log(`The app is runnning at http://localhost:${port}`);
+      }catch (error) {
+        console.log('some went wrong, really wrong!!!', error);
+    }
+})
---
+++ src/db/db.config.js
@@ -0,0 +1,12 @@
+import { CURSOR_FLAGS, MongoClient, ServerApiVersion } from "mongodb";
+import 'dotenv/config'
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v31eu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+export const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
